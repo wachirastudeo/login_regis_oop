49099ba31342c09de080f4ce930fd535babfd9dc
wachira <wachirastudeo@gmail.com>
2024-12-08 10:49:30 +0700

    add sigup.php

--- signup.php
+++ signup.php
@@ -3,8 +3,13 @@
 <?php include_once 'nav.php'; ?>
 
 <div class="container container-limited mt-5">
-    <h3 class="my-3 text-center"> Login Page</h3>
+    <h3 class="my-3 text-center"> Register Page</h3>
     <form action="">
+        <div class="mb-3">
+            <label for="name" class="form-label">Name</label>
+            <input type="name" class="form-control" name="name" aria-activedescendant="name"
+                placeholder="Enter your name">
+        </div>
         <div class="mb-3">
             <label for="email address" class="form-label">Email Address</label>
             <input type="email" class="form-control" name="email" aria-activedescendant="email"
@@ -15,13 +20,18 @@
             <input type="password" class="form-control" name="password" aria-activedescendant="password"
                 placeholder="Enter your password">
         </div>
-        <button type="submit" class="btn btn-primary">Sign In</button>
+        <div class="mb-3">
+            <label for="confirm password" class="form-label">Confirm Password</label>
+            <input type="password" class="form-control" name="confirm_password" aria-activedescendant="confirm password"
+                placeholder="Confirm password">
+        </div>
+        <button type="submit" class="btn btn-primary">Sign Up</button>
         <a href="index.php" class="btn btn-secondary">Go Back</a>
 
     </form>
     <hr>
 
-    <p class="my-3"> Already have an account? go to <a href="signup.php">sign up</a></p>
+    <p class="my-3"> Not have an account ? go to <a href="signin.php">sign in</a></p>
 </div>
 <style>
 .container-limited {
