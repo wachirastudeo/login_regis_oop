b11b91c62cee4a6089f5069596de91901c79786e
wachira <wachirastudeo@gmail.com>
2024-12-08 10:04:23 +0700

    setup project register

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php include_once 'header.php'; ?>
+
+<?php include_once 'nav.php'; ?>
+
+<div class="container">
+    <h1 class="dislay-4"> Regis OOP</h1>
+</div>
+
+<?php include_once 'footer.php'; ?>
---
+++ nav.php
@@ -0,0 +1,25 @@
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01"
+            aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+            <a class="navbar-brand" href="#">Regis OOP</a>
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+                </li>
+
+            </ul>
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="signin.php">Sign in</a>
+                </li>
+                <li class="nav-item">
+                    <a class="btn btn-primary" href="signup.php">Sign up</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
